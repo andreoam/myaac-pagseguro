483ba22eff8f84733c8a5a0eb14051ec9a8ea5c6
André Morais <andreoam@gmail.com>
2024-03-28 03:56:01 -0300

    Update install.sql

--- plugins/pagseguro/install.sql
+++ plugins/pagseguro/install.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `pagseguro_transactions` (
+CREATE TABLE IF NOT EXISTS `pagseguro_transactions` (
    `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
    `transaction_code` VARCHAR(50) NOT NULL,
    `account_id` int(11) UNSIGNED NOT NULL,
@@ -22,8 +22,7 @@ CREATE TABLE `pagseguro_transactions` (
    INDEX `delivered` (`delivered`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
-
-CREATE TABLE `myaac_send_items` (
+CREATE TABLE IF NOT EXISTS `myaac_send_items` (
    `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
    `transaction_code` VARCHAR(50) NOT NULL,
    `item_id` VARCHAR(20) NOT NULL,
@@ -44,3 +43,58 @@ CREATE TABLE `myaac_send_items` (
    INDEX `payment_method` (`payment_method`),
    INDEX `payment_status` (`payment_status`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE IF NOT EXISTS `z_ots_comunication` (
+	`id` INT(11) NOT NULL auto_increment,
+	`name` VARCHAR(255) NOT NULL DEFAULT '',
+	`type` VARCHAR(255) NOT NULL DEFAULT '',
+	`action` VARCHAR(255) NOT NULL DEFAULT '',
+	`param1` VARCHAR(255) NOT NULL DEFAULT '',
+	`param2` VARCHAR(255) NOT NULL DEFAULT '',
+	`param3` VARCHAR(255) NOT NULL DEFAULT '',
+	`param4` VARCHAR(255) NOT NULL DEFAULT '',
+	`param5` VARCHAR(255) NOT NULL DEFAULT '',
+	`param6` VARCHAR(255) NOT NULL DEFAULT '',
+	`param7` VARCHAR(255) NOT NULL DEFAULT '',
+	`delete_it` INT(2) NOT NULL DEFAULT '1',
+	PRIMARY KEY  (`id`)
+) ENGINE=MyISAM;
+
+CREATE TABLE IF NOT EXISTS `z_shop_categories` (
+	`id` INT(11) NOT NULL,
+	`name` VARCHAR(32) NOT NULL,
+	`hidden` TINYINT(1) NOT NULL DEFAULT 0,
+	PRIMARY KEY  (`id`)
+) ENGINE=MyISAM;
+
+CREATE TABLE IF NOT EXISTS `z_shop_history` (
+	`id` INT(11) NOT NULL auto_increment,
+	`comunication_id` INT(11) NOT NULL DEFAULT 0,
+	`to_name` VARCHAR(255) NOT NULL DEFAULT 0,
+	`to_account` INT(11) NOT NULL DEFAULT 0,
+	`from_nick` VARCHAR(255) NOT NULL DEFAULT '',
+	`from_account` INT(11) NOT NULL DEFAULT 0,
+	`price` INT(11) NOT NULL DEFAULT 0,
+	`offer_id` INT(11) NOT NULL DEFAULT 0,
+	`trans_state` VARCHAR(255) NOT NULL,
+	`trans_start` INT(11) NOT NULL DEFAULT 0,
+	`trans_real` INT(11) NOT NULL DEFAULT 0,
+	`is_pacc` TINYINT(1) NOT NULL DEFAULT 0,
+	PRIMARY KEY  (`id`)
+) ENGINE=MyISAM;
+
+CREATE TABLE IF NOT EXISTS `z_shop_offer` (
+	`id` INT(11) NOT NULL auto_increment,
+	`points` INT(11) NOT NULL DEFAULT 0,
+	`itemid1` INT(11) NOT NULL DEFAULT 0,
+	`count1` INT(11) NOT NULL DEFAULT 0,
+	`itemid2` INT(11) NOT NULL DEFAULT 0,
+	`count2` INT(11) NOT NULL DEFAULT 0,
+	`category_id` TINYINT(1) NOT NULL DEFAULT 0,
+	`offer_type` VARCHAR(255) DEFAULT NULL,
+	`offer_description` text NOT NULL,
+	`offer_name` VARCHAR(255) NOT NULL DEFAULT '',
+	`hidden` TINYINT(1) NOT NULL DEFAULT 0,
+	`ordering` INT(11) NOT NULL DEFAULT 0,
+	PRIMARY KEY  (`id`)
+) ENGINE=MyISAM;
